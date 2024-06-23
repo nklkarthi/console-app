7a8ff083ad3d019b681bd448044bbd0ecf1b9db3
nklkarthi <nklkarthi@gmail.com>
2024-06-23 19:58:33 +0800

    Update ci.yml to run the app

--- src/main/java/dev/nklkarthi/console/ConsoleApplication.java
+++ src/main/java/dev/nklkarthi/console/ConsoleApplication.java
@@ -1,13 +1,20 @@
 package dev.nklkarthi.console;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
-public class ConsoleApplication {
+public class ConsoleApplication implements CommandLineRunner {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ConsoleApplication.class, args);
+    public static void main(String[] args) {
+        SpringApplication.run(ConsoleApplication.class, args);
+    }
+
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println("Hello World");
 	}
-
 }
